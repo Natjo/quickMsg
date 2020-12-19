0a0fd5bb2815d3e3b0239ce40eb3a575c891df83
Jonathan MARTIN <j.martin@lonsdale.fr>
2020-12-19 18:21:00 +0100

    access

--- quickMsg.js
+++ quickMsg.js
@@ -1,6 +1,18 @@
+/**
+ * @module QuickMsg
+ * @description 
+ * Add message in a small box
+ *
+ * @param content - message to show
+ * @param duration - duration of showing box
+ * 
+ */
+
 const QuickMsg = (content, duration) => {
 	const el = document.createElement('div');
 	el.className = 'quickMsg';
+	el.setAttribute('role', 'dialog');
+	el.setAttribute('aria-modal', true);
 	el.innerHTML = content;
 	document.body.appendChild(el);
 	setTimeout(() => {
